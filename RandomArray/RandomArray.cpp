87aadfe960dd05f40aee63fecc93df220f95d2a5
Владимир Солдатов <cbb110@rambler.ru>
2022-05-12 17:01:21 +0300

    Добавьте файлы проекта.

---
+++ RandomArray/RandomArray.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+void show_massive(int array[], int size_array)
+{
+	for (int i = 0; i < size_array; i++)
+	{
+		cout << array[i] << "\t";
+	}
+}
+	void create_array(int array[], int size_array)
+	{
+		const int size = 20;
+		srand((unsigned int)time(NULL));
+		int massive[20];
+		for (int i = 0; i < size; i++)
+		{
+			massive[i] = rand() % 20;
+		}
+	
+	}
+int main()
+{
+	const int size = 20;
+	srand((unsigned int)time(NULL));
+	int massive[20];
+	create_array(massive, size);
+	show_massive(massive, size);
+	cout << "\n\n";
+	
+	for (int i = 0; i < size; i++)
+	{
+		massive[i] = massive[size - 1 - rand() % (size - i)];
+	}
+	show_massive(massive, size);
+
+}
